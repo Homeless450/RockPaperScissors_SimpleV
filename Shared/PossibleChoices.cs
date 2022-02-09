4fa21ea1ad17e6330d9d60c0eae8f61a3b54a53a
Homeless450 <apocaliptik450@gmail.com>
2022-02-09 16:13:16 +0300

    Add Shared Project

---
+++ Shared/PossibleChoices.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared
+{
+    public class PossibleChoices
+    {
+        public enum Decision : short
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissors = 3
+        }
+    }
+}
